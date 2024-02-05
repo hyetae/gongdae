a895b86437a550dec08a5549249941e08181ee9f
윤혜경 <sunrespect@naver.com>
2024-02-05 18:34:42 +0900

    Init: 프로젝트 환경 세팅

---
+++ server/src/test/java/com/jy/gongdae/GongdaeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jy.gongdae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GongdaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
