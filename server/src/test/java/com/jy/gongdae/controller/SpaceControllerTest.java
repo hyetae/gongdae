291887ba75e5ab517ccd2f2ad91aeb907fcb09b0
hyetae <86393948+hyetae@users.noreply.github.com>
2024-03-17 19:34:53 +0900

    Test: Space test

--- server/src/test/java/com/jy/gongdae/controller/SpaceControllerTest.java
+++ server/src/test/java/com/jy/gongdae/controller/SpaceControllerTest.java
@@ -1,4 +1,87 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.jy.gongdae.controller;
+
+import com.jy.gongdae.dto.SpaceCreateDto;
+import com.jy.gongdae.dto.SpaceReadDto;
+import com.jy.gongdae.service.SpaceServiceImpl;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Collections;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(SpaceController.class)
 class SpaceControllerTest {
-  
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    SpaceServiceImpl spaceService;
+
+    @Test
+    @DisplayName("Space 데이터 가져오기 테스트")
+    void getSpaceTest() throws Exception {
+
+        Long spaceId = 1L;
+        SpaceReadDto spaceReadDto = new SpaceReadDto();
+        spaceReadDto.setTitle("test-title");
+        spaceReadDto.setAddress("test-address");
+        spaceReadDto.setSector("test-sector");
+        spaceReadDto.setPrice(123);
+        spaceReadDto.setPurpose(0);
+
+        when(spaceService.findSpaceById(spaceId)).thenReturn(spaceReadDto);
+
+        mvc.perform(
+                get("/api/v1/space/{id}", spaceId))
+                        .andExpect(status().isOk())
+                        .andExpect(jsonPath("$.title").value("test-title"))
+                        .andExpect(jsonPath("$.address").value("test-address"))
+                        .andExpect(jsonPath("$.sector").value("test-sector"))
+                        .andExpect(jsonPath("$.price").value(123))
+                        .andExpect(jsonPath("$.purpose").value(0))
+                        .andDo(print());
+
+        verify(spaceService).findSpaceById(spaceId);
+    }
+
+    @Test
+    @DisplayName("Space 데이터 생성 테스트")
+    void createSpaceTest() throws Exception {
+
+        MockMultipartFile imageFile = new MockMultipartFile("images", "test-image.jpg",
+                MediaType.IMAGE_JPEG_VALUE, "test-image".getBytes());
+
+        SpaceCreateDto spaceCreateDto = new SpaceCreateDto();
+        spaceCreateDto.setTitle("test-title");
+        spaceCreateDto.setAddress("test-address");
+        spaceCreateDto.setSector("test-sector");
+        spaceCreateDto.setPrice(123);
+        spaceCreateDto.setPurpose(0);
+        spaceCreateDto.setImages(Collections.singletonList(imageFile));
+
+        when(spaceService.createSpace(any(SpaceCreateDto.class))).thenReturn(spaceCreateDto.toEntity());
+
+        mvc.perform(
+                post("/api/v1/space")
+                        .content(spaceCreateDto.toString())
+                        .contentType(MediaType.MULTIPART_FORM_DATA))
+                .andExpect(status().isOk())
+                .andDo(print());
+
+        verify(spaceService).createSpace(any(SpaceCreateDto.class));
+    }
 }
--- server/src/test/java/com/jy/gongdae/repository/SpaceRepoTest.java
+++ server/src/test/java/com/jy/gongdae/repository/SpaceRepoTest.java
@@ -1,20 +1,58 @@
-//package com.jy.gongdae.repository;
-//
-//import com.jy.gongdae.domain.Space;
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-//
-//@DataJpaTest
-//public class SpaceRepoTest {
-//
-//    @Autowired
-//    private SpaceRepo spaceRepo;
-//
+package com.jy.gongdae.repository;
+
+import com.jy.gongdae.domain.Space;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class SpaceRepoTest {
+
+    @Autowired
+    private SpaceRepo spaceRepo;
+
+    @Test
+    @DisplayName("Space DB save")
+    void saveSpace() {
+        Space space = Space.builder()
+                .title("Test-title")
+                .address("Test-address")
+                .sector("Test-sector")
+                .price(1234)
+                .purpose(0)
+                .build();
+
+        Space result = spaceRepo.save(space);
+
+        assertThat(result.getTitle()).isEqualTo(space.getTitle());
+        assertThat(result.getAddress()).isEqualTo(space.getAddress());
+        assertThat(result.getSector()).isEqualTo(space.getSector());
+        assertThat(result.getPrice()).isEqualTo(space.getPrice());
+        assertThat(result.getPurpose()).isEqualTo(space.getPurpose());
+    }
+
 //    @Test
-//    @DisplayName("Space DB save")
-//    void saveSpace() {
-//        Space space = new
+//    @DisplayName("Space list test")
+//    void SpaceList(){
+//        Space space = Space.builder()
+//                .title("Test-title")
+//                .address("Test-address")
+//                .sector("Test-sector")
+//                .price(1234)
+//                .purpose(0)
+//                .build();
+//
+//        spaceRepo.save(space);
+//
+//        List<Space> result = spaceRepo.findAll();
+//
+//        assertThat(result.size()).isEqualTo(1);
 //    }
-//}
+}
