9ebf05e368ca1412ecde79377605121845c5f48c
hyetae <86393948+hyetae@users.noreply.github.com>
2024-03-17 19:09:56 +0900

    Refactor: Edit annotations

--- server/src/main/java/com/jy/gongdae/domain/Space.java
+++ server/src/main/java/com/jy/gongdae/domain/Space.java
@@ -4,12 +4,14 @@ import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Getter
+@Setter
 @NoArgsConstructor
 @Table(name = "space_table")
 public class Space extends BaseTimeEntity {
--- server/src/main/java/com/jy/gongdae/dto/SpaceCreateDto.java
+++ server/src/main/java/com/jy/gongdae/dto/SpaceCreateDto.java
@@ -1,19 +1,17 @@
 package com.jy.gongdae.dto;
 
-import com.jy.gongdae.domain.Images;
 import com.jy.gongdae.domain.Space;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
 public class SpaceCreateDto {
-
-    private Long id;
     private String title;
     private String address;
     private String sector;
